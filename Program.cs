db1e5fb32bf2d34c6e9fd5001db693b37e1377a7
John Rowley <outsidethebox@eircom.net>
2021-10-20 15:30:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFile001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "c:/users/outsi/downloads";
+            string file = "jamaica.txt";
+            string fileToRead = $"{path}/{file}";
+
+            Console.WriteLine($"Reading {fileToRead}");
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileToRead))
+                {
+
+                    Console.WriteLine($"Starting to Reading {fileToRead}");
+
+                    string line ;
+
+                   
+                    while ( (line = sr.ReadLine()) is not null)
+                    {
+                        Console.WriteLine($"\t{line}");
+
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+
+            }
+
+
+        }
+    }
+}
